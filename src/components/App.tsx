278ff3edde73fcdbb547277ba3bd77fcb8203d65
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2025-10-15 14:25:37 +0200

    Set up Leaflet map (#3)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,53 @@
+import { useMemo } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+
+import type { Coordinate } from '@/components/Coordinate.ts';
+import type { CoordinateDto } from '@/components/CoordinateDto.ts';
+import type { InitialCoordinate } from '@/components/InitialCoordinate.ts';
+import MapMarker from '@/components/MapMarker.tsx';
+// TODO: Replace this import with your local JSON: '@/coordinates.json'
+import coordinatesJson from '@/coordinates.example.json';
+// TODO: Replace this import with your local JSON: '@/initial-coordinate.json'
+import initialCoordinateJson from '@/initial-coordinate.example.json';
+
 const App = () => {
+  const initialCoordinate: InitialCoordinate = useMemo(
+    () => ({
+      latitude: parseFloat(initialCoordinateJson[0].latitude),
+      longitude: parseFloat(initialCoordinateJson[0].longitude),
+    }),
+    []
+  );
+
+  const coordinates: Coordinate[] = useMemo(
+    () =>
+      (coordinatesJson as CoordinateDto[]).map((coordinateDto) => ({
+        name: coordinateDto.name,
+        latitude: parseFloat(coordinateDto.latitude),
+        longitude: parseFloat(coordinateDto.longitude),
+      })),
+    []
+  );
+
   return (
-    <div>
-      <p>pub-map</p>
-    </div>
+    <MapContainer
+      center={
+        [initialCoordinate.latitude, initialCoordinate.longitude] as [
+          number,
+          number,
+        ]
+      }
+      zoom={14}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {coordinates.map((coordinate, index) => (
+        <MapMarker key={index} coordinate={coordinate} />
+      ))}
+    </MapContainer>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 import App from '@/components/App.tsx';
 
+import './main.css';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
