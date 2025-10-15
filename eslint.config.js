d146cbfb682c14bd5ee8f8608ddc89024cea001a
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-10-15 11:11:29 +0200

    Set up import sorting plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,55 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
-export default defineConfig([
-  globalIgnores(["dist"]),
+export default tseslint.config([
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
+      reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // 1) React & frameworks
+            ['^react', '^next'],
+
+            // 2) Packages (node_modules)
+            ['^@?\\w'],
+
+            // 3) Absolute aliases (e.g. @ or src/)
+            ['^(@|src)(/.*|$)'],
+
+            // 4) Side-effect imports (e.g. polyfills, CSS resets)
+            ['^\\u0000'],
+
+            // 5) Relative imports
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+
+            // 6) Styles
+            ['^.+\\.s?css$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
     },
   },
 ]);
